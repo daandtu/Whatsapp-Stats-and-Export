f43592194968fb3d4b37137e7b04a835f17c299a
Luis Walter <luis.walter@tum.de>
2019-12-22 12:14:55 +0100

    Added message length plots

--- whatsapp-stats.py
+++ whatsapp-stats.py
@@ -53,10 +53,12 @@ def get_popular_words(dic, count):
 		
 
 class data:
+	counter = 0
 	def __init__(self, size):
 		self.m = create_matrix(3, size)
 		self.c = create_matrix(3, size)
 		self.filter_size = 0
+		self.avg_chars = None
 	def add(self, position, from_me, value):
 		self.m[2][position] += 1
 		self.m[from_me][position] += 1
@@ -64,32 +66,50 @@ class data:
 		self.c[from_me][position] += value
 	def smooth(self, filter_size):
 		self.filter_size += filter_size - 1
+		self.__calc_avg()
 		for i in range(3):
 			self.m[i] = filter(self.m[i], filter_size)
-	def plot(self, x_axis, xlabel='', ylabel='Messages', colors = 'r,g,b', save='', show=True, xrotation = 0, title = 'Whatsapp Chat Stats', linewidth = 1):
+			self.avg_chars[i] = filter(self.avg_chars[i], filter_size)
+	def plot(self, x_axis, xlabel='', ylabelm='Messages', ylabelc='Average message length', colors = 'r,g,b', save='', show=False, xrotation = 0, title = 'Whatsapp Chat Stats', linewidth = 1):
+		plt.clf()
 		x = np.linspace(x_axis[0], x_axis[1], num = len(self.m[2]))
-		self.__plot(x, xlabel, ylabel, colors, save, show, xrotation, title, linewidth)
-	def plot_dates(self, startdate, enddate, xlabel='', ylabel='Messages', colors = 'r,g,b', save='', show=True, xrotation = 0, title = 'Whatsapp Chat Stats', linewidth = 1):
+		self.__plot(x, self.m, xlabel, ylabelm, colors, save, show, xrotation, title, linewidth)
+		plt.clf()
+		if self.avg_chars is None: self.__calc_avg()
+		self.__plot(x, self.avg_chars, xlabel, ylabelc, colors, save, show, xrotation, title, linewidth)
+	def plot_dates(self, startdate, enddate, xlabel='', ylabelm='Messages', ylabelc='Average message length', colors = 'r,g,b', save='', show=False, xrotation = 0, title = 'Whatsapp Chat Stats', linewidth = 1):
+		plt.clf()
 		x = mdates.drange(startdate, enddate - timedelta(days=self.filter_size), timedelta(days=1))
 		plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
 		plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=int(len(x)/11)))
-		self.__plot(x, xlabel, ylabel, colors, save, show, xrotation, title, linewidth)
-	def __plot(self, x, xlabel, ylabel, colors, save, show, xrotation, title, linewidth):
+		self.__plot(x, self.m, xlabel, ylabelm, colors, save, show, xrotation, title, linewidth)
+		plt.clf()
+		if self.avg_chars is None: self.__calc_avg()
+		plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
+		plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=int(len(x)/11)))
+		self.__plot(x, self.avg_chars, xlabel, ylabelc, colors, save, show, xrotation, title, linewidth)
+	def __plot(self, x, y, xlabel, ylabel, colors, save, show, xrotation, title, linewidth):
 		colors = colors.split(',')
 		plt.title(title)
-		plt.plot(x, self.m[0], colors[0], linewidth=linewidth, label='you')
-		plt.plot(x, self.m[1], colors[1], linewidth=linewidth, label='me')
-		plt.plot(x, self.m[2], colors[2], linewidth=linewidth, label='total')
+		plt.plot(x, y[0], colors[0], linewidth=linewidth, label='you')
+		plt.plot(x, y[1], colors[1], linewidth=linewidth, label='me')
+		plt.plot(x, y[2], colors[2], linewidth=linewidth, label='total')
 		plt.xlabel(xlabel)
 		plt.ylabel(ylabel)
 		plt.legend()
 		plt.xticks(rotation=xrotation)
 		plt.tight_layout()
-		if len(save) > 0:
-			plt.savefig(save, dpi=300)
+		if len(save) == 0:
+			save = 'whatsapp_stats{}.png'.format(data.counter)
+			data.counter += 1
+		plt.savefig(save, dpi=300)
 		if show:
 			plt.show()
-		
+	def __calc_avg(self):
+		self.avg_chars = create_matrix(3, len(self.m[2]))
+		for i in range(3):
+			for j in range(len(self.m[2])):
+				self.avg_chars[i][j] = self.c[i][j]/self.m[i][j] if self.m[i][j] != 0 else 0
 		
 
 if __name__ == '__main__':
@@ -136,7 +156,6 @@ if __name__ == '__main__':
 	day =  data(24 * 6)
 	month = data(31)
 	total = data(day_diff)
-	print('Datediff:', day_diff)
 	words = {}
 	
 # Process messages
@@ -167,7 +186,7 @@ if __name__ == '__main__':
 	day.plot([0, 23], xlabel='Hours per day')
 	month.plot([1, 31], xlabel='Days per month')
 	total.smooth(70)
-	total.plot_dates(start, end, save='test.png', xrotation=45, linewidth=0.8)
+	total.plot_dates(start, end, xrotation=45, linewidth=0.8)
 	
 # Exit
 	co.close()
